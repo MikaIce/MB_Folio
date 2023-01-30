d15c681b340ba9dd60582c84186080972e733aba
MikaIce <michael.benoit13@gmail.com>
2023-01-29 22:47:41 -0300

    no key

--- MB_Consulting_NSI/MB_Consulting_NSI/settings.py
+++ MB_Consulting_NSI/MB_Consulting_NSI/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gefylyh*+=mhi*qooq5&nil&-8lkw^wtb5_f459)g4p#i&jain'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
